f46ea5f3bc0230b2d3dd6c6cd2bf53b3a3bb0d83
박주렁 <jurung@pplink.net>
2023-05-02 15:17:00 +0900

    fix: include dependencies (Update SDK to 0.0.12)

--- pagecall/src/main/java/com/pagecall/NativeBridge.java
+++ pagecall/src/main/java/com/pagecall/NativeBridge.java
@@ -167,7 +167,6 @@ class NativeBridge {
 
     @JavascriptInterface
     public void postMessage(String message) {
-        Log.d("Ryan123", "postMessage: " + message);
         JSONObject jsonObject = safeParseJSON(message);
 
         String action = jsonObject.optString("action", "");
