39ca12d7de67d3aac8c0e1407b2ac7eaeb1a38e3
taemini <tamini.kim@gmail.com>
2023-06-30 17:35:02 +0900

    release: 0.0.18

--- pagecall/src/main/java/com/pagecall/PagecallWebView.java
+++ pagecall/src/main/java/com/pagecall/PagecallWebView.java
@@ -17,7 +17,7 @@ import java.util.function.Consumer;
 
 // TODO: package private
 public class PagecallWebView extends WebView {
-    final static String version = "0.0.15";
+    final static String version = "0.0.18";
 
     private final static String[] defaultPagecallUrls = {"app.pagecall", "demo.pagecall", "192.168"};
     private final static String jsInterfaceName = "pagecallAndroidBridge";
