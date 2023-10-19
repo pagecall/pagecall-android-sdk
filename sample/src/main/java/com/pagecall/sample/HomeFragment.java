72a3211f53e0a332b961c6438c36936a91db893e
taemini <tamini.kim@gmail.com>
2023-10-19 19:53:23 +0900

    feat: split sample app into 2 fragments

---
+++ sample/src/main/java/com/pagecall/sample/HomeFragment.java
@@ -0,0 +1,59 @@
+package com.pagecall.sample;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import com.pagecall.PagecallWebView;
+
+public class HomeFragment extends Fragment {
+    private EditText roomIdInput;
+    private EditText accessTokenInput;
+    private EditText queryInput;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        roomIdInput = view.findViewById(R.id.room_id_input);
+        accessTokenInput = view.findViewById(R.id.access_token_input);
+        queryInput = view.findViewById(R.id.query_input);
+
+        view.findViewById(R.id.meet_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchPagecall(PagecallWebView.PagecallMode.MEET);
+            }
+        });
+
+        view.findViewById(R.id.replay_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchPagecall(PagecallWebView.PagecallMode.REPLAY);
+            }
+        });
+    }
+
+    private void launchPagecall(PagecallWebView.PagecallMode mode) {
+        String roomId = roomIdInput.getText().toString();
+        String accessToken = accessTokenInput.getText().toString();
+        String query = queryInput.getText().toString();
+
+        HomeFragmentDirections.ActionHomeFragmentToPagecallFragment action =
+                HomeFragmentDirections.actionHomeFragmentToPagecallFragment(mode.toString(), roomId, accessToken, query);
+        NavHostFragment.findNavController(HomeFragment.this).navigate(action);
+    }
+}
--- sample/src/main/java/com/pagecall/sample/MainActivity.java
+++ sample/src/main/java/com/pagecall/sample/MainActivity.java
@@ -2,102 +2,48 @@ package com.pagecall.sample;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.KeyEvent;
-import android.view.ViewGroup;
 import android.view.WindowManager;
-import android.widget.Button;
-import android.widget.FrameLayout;
-import android.widget.RelativeLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
-import com.pagecall.PagecallWebView;
-import com.pagecall.TerminationReason;
-
-public class MainActivity extends AppCompatActivity implements PagecallWebView.Listener {
-
-    private Button toggleButton;
-    private Button sendButton;
-    private PagecallWebView webView;
-    private RelativeLayout mainLayout;
-    private FrameLayout webViewContainer;
-    private boolean isWebViewVisible;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        PagecallWebView.setWebContentsDebuggingEnabled(true);
-
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setContentView(R.layout.activity_main);
+    }
 
-        toggleButton = findViewById(R.id.toggle_button);
-        sendButton = findViewById(R.id.send_button);
-
-        mainLayout = findViewById(R.id.main_layout);
-        webViewContainer = findViewById(R.id.webview_container);
-
-        isWebViewVisible = false;
-
-        toggleButton.setOnClickListener(v -> {
-            if (isWebViewVisible) {
-                // Remove the WebView from the container
-                webViewContainer.removeAllViews();
-                webView.destroy();
-                webView = null;
-                isWebViewVisible = false;
-            } else {
-                // Create and show the WebView
-                webView = new PagecallWebView(MainActivity.this);
-                webView.setLayoutParams(new ViewGroup.LayoutParams(
-                        ViewGroup.LayoutParams.MATCH_PARENT,
-                        ViewGroup.LayoutParams.MATCH_PARENT));
-                webView.load("{room_id}", "{token}", PagecallWebView.PagecallMode.MEET);
-                webView.setListener(this);
-                webViewContainer.addView(webView);
-                isWebViewVisible = true;
-            }
-        });
-        sendButton.setOnClickListener(v -> {
-            if (webView != null) {
-                webView.sendMessage("'hihi'");
-            }
-        });
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        // handle file upload
+        super.onActivityResult(requestCode, resultCode, intent);
+        PagecallFragment pagecallFragment = getPagecallFragment();
+        if (pagecallFragment != null) {
+            pagecallFragment.processActivityResult(requestCode, resultCode, intent);
+        }
     }
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if (webView.handleVolumeKeys(keyCode, event)) {
+        PagecallFragment pagecallFragment = getPagecallFragment();
+        if (pagecallFragment != null && pagecallFragment.processKeyDown(keyCode, event)) {
             return true;
         }
         return super.onKeyDown(keyCode, event);
     }
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-        super.onActivityResult(requestCode, resultCode, intent);
-        webView.onActivityResult(requestCode, resultCode, intent);
-    }
 
-    // PagecallWebView.Listener implementations
-    @Override
-    public void onLoaded() {
-        Log.d("SampleApp", "Room is loaded");
-    }
-
-    @Override
-    public void onMessage(String message) {
-        Log.d("SampleApp", "Message received: " + message);
-    }
-
-    @Override
-    public void onTerminated(TerminationReason reason) {
-        if (reason == TerminationReason.INTERNAL) {
-            Log.d("SampleApp", "Terminated with internal reason");
+    private PagecallFragment getPagecallFragment() {
+        Fragment navHostFragment = getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_content_main);
+        if (navHostFragment != null) {
+            Fragment currentFragment = navHostFragment.getChildFragmentManager().getFragments().get(0);
+            if (currentFragment instanceof PagecallFragment) {
+                return ((PagecallFragment) currentFragment);
+            }
         }
-        if (reason == TerminationReason.OTHER) {
-            Log.d("SampleApp", "Terminated with other reason (" + reason.getOtherReason() +")");
-        }
+        return null;
     }
 }
