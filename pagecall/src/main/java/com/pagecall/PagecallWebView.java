537eb4c26df8f894958d61691fc1da292ae03520
박주렁 <jurung@pplink.net>
2023-11-27 12:55:23 +0900

    chore: bump 0.0.29

--- pagecall/src/main/java/com/pagecall/PagecallWebView.java
+++ pagecall/src/main/java/com/pagecall/PagecallWebView.java
@@ -72,7 +72,7 @@ final public class PagecallWebView extends WebView {
 
     private Listener listener;
 
-    final static String version = "0.0.28";
+    final static String version = "0.0.29";
 
     private final static String[] defaultPagecallUrls = {"app.pagecall", "demo.pagecall", "192.168"};
     private final static String jsInterfaceName = "pagecallAndroidBridge";
