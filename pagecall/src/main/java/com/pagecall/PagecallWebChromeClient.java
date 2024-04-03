e9b01f26eca825e66d7e7d7bbbb6e095f5e8e9a4
박주렁 <jurung@pplink.net>
2024-04-03 12:37:36 +0900

    0.0.37-hotfix0 (IMAGE_SELECTOR_REQ = 911014)

--- pagecall/src/main/java/com/pagecall/PagecallWebChromeClient.java
+++ pagecall/src/main/java/com/pagecall/PagecallWebChromeClient.java
@@ -15,7 +15,7 @@ import java.util.ArrayList;
 
 public class PagecallWebChromeClient extends WebChromeClient {
 
-    public final static int IMAGE_SELECTOR_REQ = 1;
+    public final static int IMAGE_SELECTOR_REQ = 911014;
 
     private PagecallWebView webView;
 
