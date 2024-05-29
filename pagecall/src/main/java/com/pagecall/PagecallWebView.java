bfff72e778c3090d43c6c44e957c21eaf54de002
Jaeseong Seo <07js23@gmail.com>
2024-05-29 16:13:01 +0900

    feat: bump v0.0.39-nomi0

--- pagecall/src/main/java/com/pagecall/PagecallWebView.java
+++ pagecall/src/main/java/com/pagecall/PagecallWebView.java
@@ -75,7 +75,7 @@ final public class PagecallWebView extends WebView {
 
     private Listener listener;
 
-    final static String version = "0.0.39";
+    final static String version = "0.0.39-nomi0";
 
     private final static String[] defaultPagecallUrls = {"app.pagecall", "demo.pagecall", "192.168"};
     private final static String jsInterfaceName = "pagecallAndroidBridge";
