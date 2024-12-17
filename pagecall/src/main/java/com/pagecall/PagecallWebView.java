f95baff7830dad712ccfbf0fdd173988e76eeda7
Jaeseong Seo <07js23@gmail.com>
2024-12-17 15:18:33 +0900

    release: 0.0.44

--- pagecall/src/main/java/com/pagecall/PagecallWebView.java
+++ pagecall/src/main/java/com/pagecall/PagecallWebView.java
@@ -82,7 +82,7 @@ final public class PagecallWebView extends WebView {
 
     private Listener listener;
 
-    final static String version = "0.0.43";
+    final static String version = "0.0.44";
 
     private final static String[] defaultPagecallUrls = {"app.pagecall", "demo.pagecall", "192.168"};
     private final static String jsInterfaceName = "pagecallAndroidBridge";
